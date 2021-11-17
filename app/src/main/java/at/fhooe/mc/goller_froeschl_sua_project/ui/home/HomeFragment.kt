dffde80177b76caa2a33f9dc1cddeeb0423a02d0
Simon Goller <simon.goller4@gmail.com>
2021-11-17 10:19:37 +0100

    Initial commit

---
+++ app/src/main/java/at/fhooe/mc/goller_froeschl_sua_project/ui/home/HomeFragment.kt
@@ -0,0 +1,45 @@
+package at.fhooe.mc.goller_froeschl_sua_project.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import at.fhooe.mc.goller_froeschl_sua_project.R
+import at.fhooe.mc.goller_froeschl_sua_project.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
